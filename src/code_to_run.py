21b3a5a33425ca1ab20883c2700421f9037a6a2e
unknown <jjaragon@uninorte.edu.co>
2022-10-03 21:29:35 -0500

    Prueba del código

---
+++ src/code_to_run.py
@@ -0,0 +1,14 @@
+#%%
+from Restaurant import Product
+
+
+spaghetti = Product("food#01", "Spaghetti", "12000","Un espaguete que está bueno")
+# %%
+spaghetti.food_id
+# %%
+spaghetti.name
+# %%
+spaghetti.price
+# %%
+spaghetti.food_description
+# %%
